833df57dd98b4e2366d42f5fbff313798cc6c805
Florian Thiévent <florianjeremias@gmail.com>
2019-02-18 10:03:47 +0100

    reformat code

--- 01_Networking1/src/main/java/ch/fhnw/ds/echo/EchoClient.java
+++ 01_Networking1/src/main/java/ch/fhnw/ds/echo/EchoClient.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2019 Fachhochschule Nordwestschweiz (FHNW)
- * All Rights Reserved. 
+ * All Rights Reserved.
  */
 
 package ch.fhnw.ds.echo;
@@ -12,34 +12,39 @@ import java.net.Socket;
 
 public class EchoClient {
 
-	public static void main(String[] args) throws Exception {
-		String host = "localhost";
-		int port = 1234;
-//		host = "86.119.38.130";
-//		port = 7;
-		if (args.length > 0) { host = args[0]; }
-		if (args.length > 1) { port = Integer.parseInt(args[1]); }
-		
-		System.out.println("connecting to " + host + ":" + port);
+    public static void main(String[] args) throws Exception {
+         String host = "localhost";
+         int port = 1234;
+		// String host = "86.119.38.130";
+        // int port = 7;
 
-		Socket s = new Socket(host, port, null, 0);
-		// null means any network interface
-		// 0 means any port
+        if (args.length > 0) {
+            host = args[0];
+        }
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]);
+        }
 
-		System.out.println("connected to " + s.getRemoteSocketAddress());
+        System.out.println("connecting to " + host + ":" + port);
 
-		PrintWriter out = new PrintWriter(s.getOutputStream());
-		BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        Socket s = new Socket(host, port, null, 0);
+        // null means any network interface
+        // 0 means any port
 
-		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-		String input = stdin.readLine();
-		while (input != null && !input.equals("")) {
-			out.println(input);
-			out.flush();
-			System.out.println("Echo: " + in.readLine());
-			input = stdin.readLine();
-		}
-		s.close();
-		System.out.println("disconnected.");
-	}
+        System.out.println("connected to " + s.getRemoteSocketAddress());
+
+        PrintWriter out = new PrintWriter(s.getOutputStream());
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
+        String input = stdin.readLine();
+        while (input != null && !input.equals("")) {
+            out.println(input);
+            out.flush();
+            System.out.println("Echo: " + in.readLine());
+            input = stdin.readLine();
+        }
+        s.close();
+        System.out.println("disconnected.");
+    }
 }
--- 01_Networking1/src/main/java/ch/fhnw/ds/networking/tcp/Mailer.java
+++ 01_Networking1/src/main/java/ch/fhnw/ds/networking/tcp/Mailer.java
@@ -9,46 +9,46 @@ import java.util.Scanner;
 
 public class Mailer {
 
-	static Scanner in;
-	static Writer  out;
-	
-	public static void main(String[] args) throws Exception {
-		Socket s = new Socket("lmailer.fhnw.ch", 25);
+    static Scanner in;
+    static Writer out;
 
-		in  = new Scanner(s.getInputStream());
-		out = new PrintWriter(s.getOutputStream(), true /* autoFlush */);
+    public static void main(String[] args) throws Exception {
+        Socket s = new Socket("lmailer.fhnw.ch", 25);
 
-		String hostName = InetAddress.getLocalHost().getHostName();
-		System.out.println(hostName);
+        in = new Scanner(s.getInputStream());
+        out = new PrintWriter(s.getOutputStream(), true /* autoFlush */);
 
-		receive();
-		send("HELO " + hostName);
-		receive();
-		send("MAIL FROM: <juerg.christener@fhnw.ch>");
-		receive();
-		send("RCPT TO: <dominik.gruntz@fhnw.ch>");
-		receive();
-		send("DATA");
-		receive();
-		send("To: dominik.gruntz@fhnw.ch");
-		send("Subject: Meeting");
-		send("Hallo Dominik, komm doch rasch in mein Buero.");
-		send(".");
-		receive();
-		s.close();
-	}
+        String hostName = InetAddress.getLocalHost().getHostName();
+        System.out.println(hostName);
 
-	private static void receive() throws IOException {
-		if (in.hasNext()) {
-			String line = in.nextLine();
-			System.err.println(line);
-		}
-	}
+        receive();
+        send("HELO " + hostName);
+        receive();
+        send("MAIL FROM: <juerg.christener@fhnw.ch>");
+        receive();
+        send("RCPT TO: <dominik.gruntz@fhnw.ch>");
+        receive();
+        send("DATA");
+        receive();
+        send("To: dominik.gruntz@fhnw.ch");
+        send("Subject: Meeting");
+        send("Hallo Dominik, komm doch rasch in mein Buero.");
+        send(".");
+        receive();
+        s.close();
+    }
 
-	public static void send(String s) throws IOException {
-		System.out.println(s);
-		out.write(s.replaceAll("\n", "\r\n"));
-		out.write("\r\n");
-		out.flush();
-	}
+    private static void receive() throws IOException {
+        if (in.hasNext()) {
+            String line = in.nextLine();
+            System.err.println(line);
+        }
+    }
+
+    public static void send(String s) throws IOException {
+        System.out.println(s);
+        out.write(s.replaceAll("\n", "\r\n"));
+        out.write("\r\n");
+        out.flush();
+    }
 }
